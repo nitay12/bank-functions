20cf54388956defabc5f331db87285a04136c31e
Nitay Caspi <NitayCaspi12@gmail.com>
2024-05-07 14:56:01 +0300

    fix tests

--- test_bank_interface.py
+++ test_bank_interface.py
@@ -5,21 +5,20 @@ class TestBankInterface(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_initial_balance(self):
+    def test_a_initial_balance(self):
         self.assertEqual(check_balance(), 0)
 
-    def test_deposit_funds(self):
+    def test_b_deposit_funds(self):
         deposit(100)
         self.assertEqual(check_balance(), 100)
 
-    def test_withdraw_funds(self):
-        deposit(100)
+    def test_c_withdraw_funds(self):
         withdraw(50)
         self.assertEqual(check_balance(), 50)
 
-    def test_withdraw_more_than_balance(self):
-        deposit(100)
+    def test_d_withdraw_more_than_balance(self):
         self.assertFalse(withdraw(150))
 
 if __name__ == '__main__':
     unittest.main()
+    
